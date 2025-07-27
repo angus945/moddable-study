f0fe5192cde1bd4e19b5cb27ca1597f97153c7e6
angus945 main Desktop <angus9456@gmail.com>
2025-07-27 09:42:19 +0800

    move test to .Example

---
+++ Assets/Script/AngusChangyiMods.Example/Tests/DefinitionLoaderTests.cs
@@ -0,0 +1,17 @@
+
+using NUnit.Framework;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using AngusChangyiMods.Core;
+using AngusChangyiMods.Core.Utils;
+
+namespace AngusChangyiMods.Tests
+{
+    public class DefinitionInheritorTests
+    {
+
+    }
+}

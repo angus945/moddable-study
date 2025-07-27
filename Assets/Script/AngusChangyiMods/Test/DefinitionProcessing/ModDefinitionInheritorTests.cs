e5553b76789d2d42ff8e5f65e8749f1bbc28a8e5
angusChan <47582598+angus945@users.noreply.github.com>
2025-07-27 16:11:56 +0800

    refactor tests to use TestCaseSource

--- Assets/Script/AngusChangyiMods/Test/DefinitionProcessing/ModDefinitionInheritorTests.cs
+++ Assets/Script/AngusChangyiMods/Test/DefinitionProcessing/ModDefinitionInheritorTests.cs
@@ -12,31 +12,17 @@ namespace ModInfrastructure.Test
   public class ModDefinitionInheritorTests
   {
     private ModDefinitionInheritor inheritor;
-    private string testDirectory;
 
     [SetUp]
     public void SetUp()
     {
       inheritor = new ModDefinitionInheritor(new NullLogger());
-      testDirectory = Path.Combine(Path.GetTempPath(), "ModDefinitionInheritorTests", Guid.NewGuid().ToString());
-      Directory.CreateDirectory(testDirectory);
     }
 
     [TearDown]
-    public void TearDown()
-    {
-      if (Directory.Exists(testDirectory))
-        Directory.Delete(testDirectory, true);
-    }
+    public void TearDown() { }
 
     #region Helper
-
-    private string CreateTestXmlFile(string fileName, string xmlContent)
-    {
-      string filePath = Path.Combine(testDirectory, fileName);
-      File.WriteAllText(filePath, xmlContent);
-      return filePath;
-    }
 
     private XDocument LoadTestXml(string xml)
     {
@@ -45,25 +31,10 @@ namespace ModInfrastructure.Test
 
     #endregion
 
-    [Test]
-    public void Test_01_SingleInheritance_ShouldResolveAndMergeFields()
+    [TestCaseSource(typeof(ModDefinitionInheritorTestCases), nameof(ModDefinitionInheritorTestCases.SingleInheritance))]
+    public void Test_01_SingleInheritance_ShouldResolveAndMergeFields(string xml)
     {
       // Arrange
-      var xml = @"
-<Defs>
-  <ThingDef isAbstract=""true"">
-    <defID>BaseItem</defID>
-    <label>基礎物品</label>
-    <description>物品的基本描述</description>
-    <stack>1</stack>
-  </ThingDef>
-  <ThingDef parent=""BaseItem"">
-    <defID>HealthPotion</defID>
-    <label>治療藥水</label>
-    <stack>5</stack>
-  </ThingDef>
-</Defs>";
-
       var doc = LoadTestXml(xml);
 
       // Act
@@ -78,25 +49,10 @@ namespace ModInfrastructure.Test
       Assert.AreEqual("5", potion.Element("stack")?.Value); // 子級覆蓋
     }
 
-    [Test]
-    public void Test_02_DeepInheritance_ShouldResolveMultiLevelChain()
+    [TestCaseSource(typeof(ModDefinitionInheritorTestCases), nameof(ModDefinitionInheritorTestCases.DeepInheritance))]
+    public void Test_02_DeepInheritance_ShouldResolveMultiLevelChain(string xml)
     {
       // Arrange
-      var xml = @"
-<Defs>
-  <ThingDef isAbstract=""true"">
-    <defID>BaseItem</defID>
-    <stack>1</stack>
-  </ThingDef>
-  <ThingDef isAbstract=""true"" parent=""BaseItem"">
-    <defID>BaseWeapon</defID>
-    <damage>10</damage>
-  </ThingDef>
-  <ThingDef parent=""BaseWeapon"">
-    <defID>IronSword</defID>
-    <damage>30</damage>
-  </ThingDef>
-</Defs>";
       var doc = LoadTestXml(xml);
 
       // Act
@@ -110,19 +66,10 @@ namespace ModInfrastructure.Test
       Assert.AreEqual("30", sword.Element("damage")?.Value, "應覆蓋 BaseWeapon 的 damage");
     }
 
-    [Test]
-    public void Test_03_AbstractDefs_ShouldBeRemoved()
+    [TestCaseSource(typeof(ModDefinitionInheritorTestCases), nameof(ModDefinitionInheritorTestCases.AbstractDefs))]
+    public void Test_03_AbstractDefs_ShouldBeRemoved(string xml)
     {
       // Arrange
-      var xml = @"
-<Defs>
-  <ThingDef isAbstract=""true"">
-    <defID>AbstractDef</defID>
-  </ThingDef>
-  <ThingDef>
-    <defID>ConcreteDef</defID>
-  </ThingDef>
-</Defs>";
       var doc = LoadTestXml(xml);
 
       // Act
@@ -134,27 +81,10 @@ namespace ModInfrastructure.Test
       Assert.IsFalse(allDefIDs.Contains("AbstractDef"), "抽象定義應被移除");
     }
 
-    [Test]
-    public void Test_04_TagLists_ShouldBeMergedAndDeduplicated()
+    [TestCaseSource(typeof(ModDefinitionInheritorTestCases), nameof(ModDefinitionInheritorTestCases.TagLists))]
+    public void Test_04_TagLists_ShouldBeMergedAndDeduplicated(string xml)
     {
       // Arrange
-      var xml = @"
-<Defs>
-  <ThingDef isAbstract=""true"">
-    <defID>Base</defID>
-    <tags>
-      <tag>a</tag>
-      <tag>b</tag>
-    </tags>
-  </ThingDef>
-  <ThingDef parent=""Base"">
-    <defID>Child</defID>
-    <tags>
-      <tag>b</tag>
-      <tag>c</tag>
-    </tags>
-  </ThingDef>
-</Defs>";
       var doc = LoadTestXml(xml);
 
       // Act
@@ -171,17 +101,10 @@ namespace ModInfrastructure.Test
       CollectionAssert.AreEquivalent(new[] { "a", "b", "c" }, tags);
     }
 
-    [Test]
-    public void Test_05_MissingParent_ShouldIgnoreAndSkip()
+    [TestCaseSource(typeof(ModDefinitionInheritorTestCases), nameof(ModDefinitionInheritorTestCases.MissingParent))]
+    public void Test_05_MissingParent_ShouldIgnoreAndSkip(string xml)
     {
       // Arrange
-      var xml = @"
-<Defs>
-  <ThingDef parent=""NonExistent"">
-    <defID>Orphan</defID>
-    <label>遺失繼承</label>
-  </ThingDef>
-</Defs>";
       var doc = LoadTestXml(xml);
 
       // Act
